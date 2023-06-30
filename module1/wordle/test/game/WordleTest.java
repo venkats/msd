5708768b9fded190a92ba3b8f4db0e7d1abc52b5
venkats@agiledeveloper.com <venkats@agiledeveloper.com>
2023-06-30 07:52:45 -0600

    initial project with a canary test

---
+++ module1/wordle/test/game/WordleTest.java
@@ -0,0 +1,13 @@
+package game;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class WordleTest {
+  @Test
+  void canary(){
+    assertTrue(true);
+  }
+
+}
