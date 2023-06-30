d641aedd21c9617facd4f1f6e71ff00a78b00460
venkats@agiledeveloper.com <venkats@agiledeveloper.com>
2023-06-30 15:47:09 -0600

    tests for the actual dependency

---
+++ module2/wordle/src/game/AgileDeveloperSpellChecker.java
@@ -0,0 +1,24 @@
+package game;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.util.Scanner;
+
+public class AgileDeveloperSpellChecker {
+  public String getResponse(String word) throws IOException {
+    var url = "http://agiledeveloper.com/spellcheck?check=" + word;
+
+    try(var scanner = new Scanner(URI.create(url).toURL().openStream())) {
+      return scanner.nextLine();
+    }
+  }
+
+  public boolean isSpellingCorrect(String word) {
+    try {
+      return getResponse(word).equals("true");
+    } catch (IOException ex) {
+      throw new RuntimeException(ex.getMessage(), ex);
+    }
+  }
+}
---
+++ module2/wordle/test/game/AgileDeveloperSpellCheckerTest.java
@@ -0,0 +1,47 @@
+package game;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+public class AgileDeveloperSpellCheckerTest {
+  private AgileDeveloperSpellChecker agileDeveloperSpellChecker;
+
+  @BeforeEach
+  void init() {
+    agileDeveloperSpellChecker = spy(new AgileDeveloperSpellChecker());
+  }
+
+  @Test
+  void getResponseFromServiceForAWord() throws IOException {
+    assertTrue(agileDeveloperSpellChecker.getResponse("RIVER").length() > 0);
+  }
+
+  @Test
+  void isSpellingorrectForCorrectSpelling() throws IOException {
+    assertTrue(agileDeveloperSpellChecker.isSpellingCorrect("good"));
+
+    verify(agileDeveloperSpellChecker, times(1)).getResponse("good");
+  }
+
+  @Test
+  void isSpellingCorrectForIncorrectSpelling() throws IOException {
+    assertFalse(agileDeveloperSpellChecker.isSpellingCorrect("gddo"));
+
+    verify(agileDeveloperSpellChecker, times(1)).getResponse("gddo");
+  }
+
+  @Test
+  void isSpellingCorrectWithException() throws IOException {
+    when(agileDeveloperSpellChecker.getResponse("gddo")).thenThrow(new IOException("Network failure"));
+
+    var ex = assertThrows(RuntimeException.class, () -> agileDeveloperSpellChecker.isSpellingCorrect("gddo"));
+
+    assertEquals("Network failure", ex.getMessage());
+    verify(agileDeveloperSpellChecker, times(1)).getResponse("gddo");
+  }
+}
